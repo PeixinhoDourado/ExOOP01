bc43038471bce57a736b0573df1e289bafe18325
Peixinho Dourado <mika.marcondes2@gmail.com>
2019-04-18 17:37:08 -0300

    Encapsulamento e construtores criados

--- ExOOP01/Models/Cliente.cs
+++ ExOOP01/Models/Cliente.cs
@@ -8,10 +8,25 @@ namespace ExOOP01.Models
 {
     public class Cliente
     {
-       private int codigo;
-       private string nome;
-       private string email;
-       private string cidade;
-       private string uf;
+        private int codigo;
+        private string nome;
+        private string email;
+        private string cidade;
+        private string uf;
+
+        public int Codigo { get => codigo; set => codigo = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Email { get => email; set => email = value; }
+        public string Cidade { get => cidade; set => cidade = value; }
+        public string Uf { get => uf; set => uf = value; }
+
+        public Cliente(int codigo, string nome, string email, string cidade, string uf)
+        {
+            this.codigo = codigo;
+            this.nome = nome;
+            this.email = email;
+            this.cidade = cidade;
+            this.uf = uf;
+        }
     }
 }
--- ExOOP01/Models/Pedido.cs
+++ ExOOP01/Models/Pedido.cs
@@ -16,6 +16,25 @@ namespace ExOOP01.Models
         private double valorUnitario;
         private int quantidade;
 
+        public int Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        public DateTime DataHora
+        {
+            get { return dataHora; }
+            set { dataHora = value; }
+        }
+
+        public string Observacoes { get => observacoes; set => observacoes = value; }
+        public double Valor { get => valor; set => valor = value; }
+        public Cliente Cliente { get => cliente; set => cliente = value; }
+        public double ValorUnitario { get => valorUnitario; set => valorUnitario = value; }
+        public int Quantidade { get => quantidade; set => quantidade = value; }
+
+
         public Pedido(int codigo, DateTime dataHora, string observacoes, double valor, Cliente cliente, double valorUnitario, int quantidade )
         {
             this.codigo = codigo;
