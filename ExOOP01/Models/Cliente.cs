ff64325ffa521cf873a0db46e8e9eb9dd09ebcc8
Peixinho Dourado <mika.marcondes2@gmail.com>
2019-04-18 14:44:00 -0300

    Classe Cliente criada

---
+++ ExOOP01/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExOOP01.Models
+{
+    class Cliente
+    {
+        int codigo;
+        string nome;
+        string email;
+        string cidade;
+        string uf;
+    }
+}
