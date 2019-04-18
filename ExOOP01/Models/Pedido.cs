e5d15e1b18a7236dff62222c99d8458b91208574
Peixinho Dourado <mika.marcondes2@gmail.com>
2019-04-18 14:48:10 -0300

    Classe Pedido criada

---
+++ ExOOP01/Models/Pedido.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExOOP01.Models
+{
+    class Pedido
+    {
+        int codigo;
+        DateTime dataHora;
+        string observacoes;
+        double valor;
+        Cliente cliente;
+        
+    }
+}
